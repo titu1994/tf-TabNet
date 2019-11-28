185455b1eb4810f5c98f7712eaca7c93cb769e9d
Somshubra Majumdar <titu1994@gmail.com>
2019-11-28 09:49:33 -0600

    Fix bug where module tabnet is not importable

--- tabnet/__init__.py
+++ tabnet/__init__.py
@@ -12,7 +12,7 @@ from tabnet.stacked_tabnet import StackedTabNetRegressor
 tabnets = ['TabNet', 'TabNetClassifier', 'TabNetRegressor']
 stacked_tabnets = ['StackedTabNet', 'StackedTabNetClassifier', 'StackedTabNetRegressor']
 
-__all__ = [*tabnets, *stacked_tabnet]
+__all__ = [*tabnets, *stacked_tabnets]
 
 
-__version__ = '0.1.4'
+__version__ = '0.1.4.1'
